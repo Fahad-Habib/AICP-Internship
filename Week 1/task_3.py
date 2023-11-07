67841a3e691a29d8e6b0b54491aff48dd69281cf
Fahad-Habib <mr.fahadhabib@gmail.com>
2023-11-08 01:28:35 +0500

    Week 1 Task 3

---
+++ Week 1/task_3.py
@@ -0,0 +1,57 @@
+from tabulate import tabulate
+
+
+with open('data.csv') as file:
+	data = [i.split(',') for i in file.read().strip().split('\n')]
+	items = {i[1]: [i[0], i[2], float(i[3])] for i in data[1:]}
+
+
+print(tabulate(data, headers='firstrow', tablefmt='fancy_grid'), '\n')
+
+case = input('Please choose a case and enter the item code: ').upper()
+while case[0] != 'A' or case not in items:
+	print("[INVALID] Invalid code")
+	case = input('Please enter a valid item code for a case: ').upper()
+
+ram = input('Please choose a RAM and enter the item code: ').upper()
+while ram[0] != 'B' or ram not in items:
+	print("[INVALID] Invalid code")
+	ram = input('Please enter a valid item code for a RAM: ').upper()
+
+main_drive = input('Please choose a Main Hard Disk Drive and enter the item code: ').upper()
+while main_drive[0] != 'C' or main_drive not in items:
+	print("[INVALID] Invalid code")
+	main_drive = input('Please enter a valid item code for a Main Hard Disk Drive: ').upper()
+
+done = 'ABC'
+extras = []
+extra = input("Please choose any additional item and enter the item code, otherwise enter 0: ").upper()
+while extra != '0':
+	if extra not in items:
+		print("[INVALID] Invalid code")
+		extra = input('Please enter a valid item code or enter 0 if you do not want anything else: ').upper()
+	elif extra[0] in done:
+		extra = input('You have already bought an item from this category please select item from any other category if you want, else enter 0: ').upper()
+	else:
+		extras.append(extra)
+		done += extra[0]
+		extra = input("Please choose any additional item and enter the item code, otherwise enter 0: ").upper()
+
+price = items[case][-1] + items[ram][-1] + items[main_drive][-1] + sum([items[i][-1] for i in extras])
+purchases = [
+	['Category', 'Description', 'Price ($)'],
+	items[case],
+	items[ram],
+	items[main_drive],
+] + [items[i] for i in extras]
+purchases.append(['Total Cost', '', f'{price} $'])
+if extras:
+	if len(extras) == 1:
+		discount = round(price * 0.05, 2)
+	else:
+		discount = round(price * 0.1, 2)
+	purchases.append(['Discount', '', f'-{discount} $'])
+	purchases.append(['Final Cost', '', f'{round(price-discount, 2)} $'])
+
+print()
+print(tabulate(purchases, headers='firstrow', tablefmt='fancy_grid'))
